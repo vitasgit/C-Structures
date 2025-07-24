fc09c9cf8a628643ecddf0dcd8c87abbc7c21b04
vitasgit <vibrantfeynman@gmail.com>
2025-07-24 16:40:59 +0500

    шаблон для bubble_sort

--- Лаб 3. Сортировка/main.c
+++ Лаб 3. Сортировка/main.c
@@ -1,10 +1,12 @@
 #include "sortunit.h"
 #include <stdio.h>
 #include <stdlib.h>
+#define N 10
 
 
 int main()
 {
-    bubble_sort();
+    int arr[N] = {1, 6, 3, 3, 5, 4, 5, 9, 8, 5};
+    bubble_sort(arr, N, sizeof(arr[0]), cmp);
     return 0;
 }
