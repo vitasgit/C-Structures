b8c2408a6407e2b47627b685acd345a1d04626b0
vitasgit <vibrantfeynman@gmail.com>
2025-08-07 11:38:38 +0500

    добавил printArr()

--- Лаб 3. Сортировка/main.c
+++ Лаб 3. Сортировка/main.c
@@ -3,11 +3,14 @@
 #include <stdlib.h>
 #define N 10
 
+void printArr(const int *parr, int size);
+
 
 int main()
 {
     int arr[N] = {1, 6, 3, 3, 5, 4, 5, 9, 8, 5};
-    bubble_sort(arr, N, sizeof(arr[0]), cmp);
+    //bubble_sort(arr, N, sizeof(arr[0]), cmp);
+    printArr(arr, N);
 
     // проверка cmp
     // int a = 5;
@@ -18,4 +21,13 @@ int main()
 
 
     return 0;
+}
+
+
+void printArr(const int *parr, int size)
+{
+	for (int i = 0; i < size; i++) {
+		printf("%d ", parr[i]);
+	}
+    printf("\n");
 }
